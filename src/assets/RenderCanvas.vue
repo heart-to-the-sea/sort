8e92df537d73802aa9d8fb7a1c01da66e5367ea8
Your Name <you@example.com>
2023-08-27 20:03:41 +0800

    init

---
+++ src/assets/RenderCanvas.vue
@@ -0,0 +1,45 @@
+<template>
+  <canvas id="canvas" ref="canvas"></canvas>
+</template>
+<script setup lang="ts">
+// import { renderList, watch, watchEffect, ref } from "vue";
+
+// const count = ref(0);
+// const canvas = ref<HTMLCanvasElement>();
+// const context = ref<CanvasRenderingContext2D | null>();
+
+// defineProps({
+//   renderList: number[]
+// });
+
+// const draw = () => {
+//   // console.time("renderTime");
+//   const ctx = context.value;
+//   if (!ctx) return;
+//   ctx.clearRect(0, 0, 1000, 1000);
+//   ctx.beginPath();
+//   renderList.forEach((node, i) => {
+//     const x = node * Math.cos((Math.PI / (length / 2)) * i);
+//     const y = node * Math.sin((Math.PI / (length / 2)) * i);
+//     ctx.moveTo(500 + x, 500 + y);
+//     ctx.fillStyle = "#000";
+//     ctx.arc(500 + x, 500 + y, 2, 0, Math.PI * 2);
+//     ctx.moveTo(500, 500);
+//     ctx.stroke();
+//   });
+//   // console.log("draw");
+//   // console.timeEnd("renderTime");
+//   return new Promise<void>((res) => {
+//     setTimeout(() => {
+//       res();
+//     });
+//   });
+// };
+</script>
+<style lang="scss">
+#canvas {
+  width: 500px;
+  height: 500px;
+  // background-color: black;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,125 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <canvas id="canvas" ref="canvas"></canvas>
+    <div>
+      <div class="button" @click="bobleStart">冒泡排序</div>
+      <div class="button" @click="insertStart">插入排序</div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+const length = 720;
+const WIDTH = 1000;
+const HEIGHT = 1000;
+const WIDTHCENTER = WIDTH / 2;
+const HEIGHTCENTER = HEIGHT / 2;
+let randomList = Array.from({ length }).map(() => Math.random() * 500);
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
+const count = ref(0);
+const canvas = ref<HTMLCanvasElement>();
+const context = ref<CanvasRenderingContext2D | null>();
+
+const draw = (changeList: number[]) => {
+  const ctx = context.value;
+  if (!ctx) return;
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  console.log(changeList);
+  randomList.forEach((node, i) => {
+    const x = node * Math.cos((Math.PI / (length / 2)) * i);
+    const y = node * Math.sin((Math.PI / (length / 2)) * i);
+    ctx.beginPath();
+    ctx.moveTo(WIDTHCENTER + x, HEIGHTCENTER + y);
+    if (changeList.includes(i)) {
+      ctx.fillStyle = "#f00";
+      ctx.strokeStyle = "#f00";
+    } else {
+      ctx.fillStyle = "#000";
+      ctx.strokeStyle = "#000";
+    }
+    ctx.arc(WIDTHCENTER + x, HEIGHTCENTER + y, 4, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.moveTo(WIDTHCENTER, HEIGHTCENTER);
+    ctx.stroke();
+  });
+  return new Promise<void>((res) => {
+    setTimeout(() => {
+      res();
+    }, 0);
+  });
+};
+
+// 冒泡排序
+async function* sort() {
+  for (let i = 0; i < randomList.length; i++) {
+    let changeList = [];
+    for (let j = 0; j < randomList.length; j++) {
+      if (randomList[j] > randomList[j + 1]) {
+        let temp = randomList[j];
+        randomList[j] = randomList[j + 1];
+        randomList[j + 1] = temp;
+        changeList.push(j);
+      }
+    }
+    await draw(changeList);
+    yield true;
+  }
+  await draw([]);
+  yield false;
+}
+
+// 插入排序
+async function* sort2() {
+  for (let i = 1; i < randomList.length; i++) {
+    let changeList = [];
+    let perIndex = i - 1;
+    let count = randomList[i];
+    while (perIndex >= 0 && randomList[perIndex] > count) {
+      randomList[perIndex + 1] = randomList[perIndex];
+      perIndex--;
+    }
+    randomList[perIndex] = count;
+    changeList.push(perIndex);
+    await draw(changeList);
+    yield true;
+  }
+  await draw([]);
+  yield false;
+}
+
+const bobleStart = async () => {
+  randomList = Array.from({ length }).map(() => Math.random() * 500);
+  let next = sort();
+  while (!(await next.next()).done);
+};
+
+const insertStart = async () => {
+  randomList = Array.from({ length }).map(() => Math.random() * 500);
+  let next = sort2();
+  while (!(await next.next()).done);
+};
+
+onMounted(async () => {
+  console.log(canvas.value);
+  context.value = canvas.value?.getContext("2d");
+  if (!canvas.value) return;
+  canvas.value.width = WIDTH;
+  canvas.value.height = HEIGHT;
+});
 </script>
+<style lang="scss" scoped>
+#canvas {
+  width: 500px;
+  height: 500px;
+  // background-color: black;
+}
+.button {
+  padding: 15px;
+  display: inline-block;
+  border-radius: 5px;
+  user-select: none;
+  cursor: pointer;
+  border: 1px solid #cecece;
+}
+</style>
